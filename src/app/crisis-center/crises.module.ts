ba1826138a5728b9b6188eadcf62b536f30ca8b2
Christian G. Fritsch <christian@rz2.com.br>
2017-04-03 10:07:18 -0300

    corrigidos pequenos erros residuais

--- src/app/crisis-center/crises.module.ts
+++ src/app/crisis-center/crises.module.ts
@@ -2,22 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { HeroesRoutingModule } from './heroes-routing.module';
-import { HeroListComponent } from './hero-list.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
+import { CrisisListComponent } from './crisis-list.component';
+import { CrisisDetailComponent } from './crisis-detail.component';
 
-import { HeroService } from './hero.service';
+import { CrisisService } from './crisis.service';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    HeroesRoutingModule
+    CrisisCenterRoutingModule
   ],
   declarations: [
-    HeroListComponent,
-    HeroDetailComponent
+    CrisisListComponent,
+    CrisisDetailComponent
   ],
-  providers: [HeroService]
+  providers: [CrisisService]
 })
-export class HeroesModule {}
+export class CrisisModule {}
